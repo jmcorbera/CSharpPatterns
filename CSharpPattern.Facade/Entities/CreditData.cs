b9057319e74f0f8a04ead91cf06d0c9778eeb686
jcorberaCP <jcorbera@credipaz.com>
2022-04-13 17:13:15 -0300

    update facade pattern

--- CSharpPattern.Facade/Entities/CreditData.cs
+++ CSharpPattern.Facade/Entities/CreditData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpPattern.Facade.Entities
+﻿namespace CSharpPattern.Facade.Entities
 {
     public class CreditData
     {
