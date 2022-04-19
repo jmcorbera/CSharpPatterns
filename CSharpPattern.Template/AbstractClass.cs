1e0942afbac445b6338fac2c0258ddda132a51d1
jmcorbera <jmcorbera@gmail.com>
2022-04-19 14:28:28 -0300

    update Template Pattern

---
+++ CSharpPattern.Template/AbstractClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharpPattern.Template
+{
+    public abstract class AbstractClass
+    {
+        public void TemplateMethod()
+        {
+            this.ConcreteOperation1();
+            this.ConcreteOperation2();
+            this.RequiredOperations1();
+            this.RequiredOperations2();
+
+            this.Hook1();
+        }
+
+        protected void ConcreteOperation1()
+        {
+            Console.WriteLine("AbstractClass says: I am doing the bulk of the work");
+        }
+
+        protected void ConcreteOperation2()
+        {
+            Console.WriteLine("AbstractClass says: But I let subclasses override some operations");
+        }
+
+        protected abstract void RequiredOperations1();
+
+        protected abstract void RequiredOperations2();
+
+        protected virtual void Hook1() { }
+
+    }
+}
---
+++ CSharpPattern.Template/ConcreteClass2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpPattern.Template
+{
+    public class ConcreteClass2 : AbstractClass
+    {
+        protected override void RequiredOperations1()
+        {
+            Console.WriteLine("ConcreteClass2 says: Implemented Operation 1");
+        }
+
+        protected override void RequiredOperations2()
+        {
+            Console.WriteLine("ConcreteClass2 says: Implemented Operation 2");
+        }
+
+        protected override void Hook1() 
+        {
+            Console.WriteLine("ConcreteClass2 says: Overridden Hook1");
+        }
+    }
+}
