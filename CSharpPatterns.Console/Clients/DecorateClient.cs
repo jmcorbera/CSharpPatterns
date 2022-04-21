d03336074cf541ae423403aa163bf5e285169c0a
jmcorbera <jmcorbera@gmail.com>
2022-04-21 12:36:50 -0300

    update console project

---
+++ CSharpPatterns.Console/Clients/DecorateClient.cs
@@ -0,0 +1,25 @@
+﻿using CSharpPattern.Decorate;
+using CSharpPattern.Decorate.Decorators;
+using System;
+
+namespace CSharpPatterns.AppConsole.Clients
+{
+    public class DecorateClient
+    {
+        public static void MainDecorate()
+        {
+            string msg = "Hello World";
+
+            NotifierMail notifier = new NotifierMail();
+            Console.WriteLine("Client: I send a simple message with original implementation :");
+            notifier.Send(msg);
+
+            Console.WriteLine("Client: I send several messages with decorator implementations :");
+
+            SMSNotifier smsNotifier = new SMSNotifier(notifier);
+            FaceBookNotifier faceBookNotifier = new FaceBookNotifier(smsNotifier);
+
+            faceBookNotifier.Send(msg);
+        }
+    }
+}
--- CSharpPatterns.Console/Program.cs
+++ CSharpPatterns.Console/Program.cs
@@ -8,17 +8,18 @@ namespace CSharpPatterns.AppConsole
         static void Main(string[] args)
         {
             TemplateMethodClient.MainTemplateMethod();
-
             Console.ReadLine();
             Console.Clear();
 
             FinancialClient.MainFinancial();
-
             Console.ReadLine();
             Console.Clear();
 
             CommandClient.MainCommand();
+            Console.ReadLine();
+            Console.Clear();
 
+            DecorateClient.MainDecorate();
             Console.ReadLine();
             Console.Clear();
 
